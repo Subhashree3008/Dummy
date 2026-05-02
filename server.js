2fc0fd465a7f2d6b2358bafb964d61c1b760b7c8
mathivarshini <mathikavitha2005@gmail.com>
2026-05-02 08:52:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const app=require('express')();
+const PORT=8080;
+
+app.listen(PORT,()=>console.log(`it is alive on http://localhost:${PORT}`));
+app.get('/tshirts',(req,res)=>{
+ res.status(200).send({
+    tshirt:'abc',
+    size:'small'
+ })
+});
